5ae5151b2ded4e791217d6b6b81278823f376d65
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-07-09 20:58:31 -0300

    Update

--- Challenge109/moeda.py
+++ Challenge109/moeda.py
@@ -1,25 +1,25 @@
 """
-Adapt the code from challenge 107, creating an additional function called moeda() that can display the values as a formatted currency value.
+Modify the functions that were created in challenge 107 so that they accept one more parameter, informing whether or not the value returned by them will be formatted by the moeda() function, developed in challenge 108.
 """
 
-def aumentar(preco = 0, taxa = 0):
+def aumentar(preco = 0, taxa = 0, formato=False):
     res= preco + (preco * taxa/100)
-    return res
+    return res if formato is False else moeda(preco)
     
-def diminuir(preco = 0, taxa = 0):
+def diminuir(preco = 0, taxa = 0, formato=False):
     res = preco - (preco * taxa/100)
-    return res
+    return res if formato is False else moeda(preco)
 
 
-def dobro(preco = 0):
+def dobro(preco = 0, formato=False):
     res = preco * 2
-    return res
+    return res if formato is False else moeda(preco)
 
     
-def metade(preco = 0):
+def metade(preco = 0, formato=False):
     res = preco / 2
-    return res  
+    return res if formato is False else moeda(preco)
 
 
-def moeda(preco = 0, moeda = 'R$'):
+def moeda(preco = 0, moeda = 'R$', formato=False):
     return f'{moeda}{preco:>.2f}'.replace('.',',')
--- Challenge109/teste.py
+++ Challenge109/teste.py
@@ -2,7 +2,7 @@ import moeda
 #from moeda import aumentar, diminuir
 
 p = float(input('Digite o preço: R$ '))
-print(f'A metade de {moeda.moeda(p)} é {moeda.moeda(moeda.metade(p))}')
-print(f'O dobro de {moeda.moeda(p)} é {moeda.moeda(moeda.dobro(p))}')
-print(f'Aumentando em 10% teremos {moeda.moeda(moeda.aumentar(p, 10))}')
-print(f'Diminuir em 20% teremos {moeda.moeda(moeda.diminuir(p, 20))}')
+print(f'A metade de {moeda.moeda(p)} é {moeda.metade(p, True)}')
+print(f'O dobro de {moeda.moeda(p)} é {moeda.dobro(p, True)}')
+print(f'Aumentando em 10% teremos {moeda.aumentar(p, 10, True)}')
+print(f'Diminuir em 13% teremos {moeda.diminuir(p, 13, True)}')  
